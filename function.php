7447ffdc75d1f175cd50189a9f92cb414d9e3689
Malikpako <x0ulhacker786@gmail.com>
2017-11-20 14:42:45 +0100

    a

---
+++ function.php
@@ -0,0 +1,12 @@
+<?php
+
+function connectionDB() {
+
+    $hostname = 'localhost';            // the credentials of the connection
+    $databasenaam = 'signIn';
+    $username = 'root';
+    $password = '';
+
+    $conn = new mysqli_connect($hostname, $username, $password, $databasenaam); // the instantiation of the mysqli object, on object TOTALLY specialised in DATABAS MANAGEMENT
+    return $conn;  // THAT object and connection is given back so that it can be catched at the call.
+}
